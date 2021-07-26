bea7ab4e53f9cc5b36fb9d61119eeeca4222c250
MIAH7 <alexha7@protonmail.com>
2021-07-26 23:45:57 +0300

    .

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,3 @@
 alias toron="sudo torghostng -u -s -p -r -mac enp1s0 --dns"
 alias toroff="sudo torghostng -x"
 alias reflectorstart="sudo reflector --verbose --protocol https --latest 20 --sort rate --save /etc/pacman.d/mirrorlist"
-alias update="paru -Syu"
--- .zshenv
+++ .zshenv
@@ -1,4 +1,5 @@
 export ZDOTDIR=$HOME/.config/zsh
+export XDG_CURRENT_DESKTOP=sway
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_WEBRENDER=0
 export QT_QPA_PLATFORM=wayland-egl
