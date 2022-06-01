d7fb6b2f5464f54cbc8db408a41ae099c46a9a75
Alexander <miah789@protonmail.com>
2022-06-01 17:49:29 +0300

    Updated zsh aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reflectorstart="sudo reflector --verbose --latest 20 --threads 5 --sort rate --save /etc/pacman.d/mirrorlist"
+alias yggdrasilstart="systemctl start yggdrasil && systemctl restart systemd-resolved"
